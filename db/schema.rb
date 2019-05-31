81d2cf866662cf67bf3ac48e3edc0f9a1c6148cd
Vince Carollo <vincecarollo@gmail.com>
2019-05-31 16:51:38 -0600

    Add location reference to orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190531000442) do
+ActiveRecord::Schema.define(version: 20190531215051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 20190531000442) do
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "location_id"
+    t.index ["location_id"], name: "index_orders_on_location_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
@@ -76,5 +78,6 @@ ActiveRecord::Schema.define(version: 20190531000442) do
   add_foreign_key "locations", "users"
   add_foreign_key "order_items", "items"
   add_foreign_key "order_items", "orders"
+  add_foreign_key "orders", "locations"
   add_foreign_key "orders", "users"
 end

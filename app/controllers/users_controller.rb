5cf8d75141fca2992781f338ddfb056fa76ecf98
Vince Carollo <vincecarollo@gmail.com>
2019-05-30 19:40:20 -0600

    Finish adding location resource upon user creation with unique location name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,18 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+    @location = Location.new
   end
 
   def create
     @user = User.new(user_params)
+    @location = Location.new(location_params)
+    @location.name = "home"
+    @location.user = @user
     if password_confirmation != true
       flash.now[:notice] = "Those passwords don't match."
       render :new
-    elsif @user.save
+    elsif @user.save && @location.save
       session[:user_id] = @user.id
       flash[:notice] = "You are now registered and logged in."
       redirect_to profile_path
@@ -30,6 +34,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :address, :city, :state, :zip, :email, :password)
+  end
+
+  def location_params
+    params.require(:user).permit(:address, :city, :state, :zip)
   end
 
 end

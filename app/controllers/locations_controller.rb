fa66a0ab646da3b87c3100c857aa05be47346e7d
Vince Carollo <vincecarollo@gmail.com>
2019-06-01 15:08:33 -0600

    Add functionality to locations controller to redirect based on current user role

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -12,22 +12,22 @@ class LocationsController < ApplicationController
     @location.user_id = @user.id
     if @location.save
       flash.notice = "Address #{@location.name} has been created"
-      redirect_to profile_path
+      render_correct
     else
       render :new
     end
   end
 
   def destroy
-    user = User.find(params[:user_id])
+    @user = User.find(params[:user_id])
     location = Location.find(params[:id])
-    if used_in_completed_order?(user, location)
+    if used_in_completed_order?(@user, location)
       flash.notice = "Your #{location.name} address is being used for a current order and cannot be deleted"
       redirect_to profile_path
     else
-      order_check(user, location) #deletes order even if on pending or canceled order
+      order_check(@user, location) #deletes order even if on pending or canceled order
       location.destroy
-      redirect_to profile_path
+      render_correct
     end
   end
 
@@ -41,13 +41,25 @@ class LocationsController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:user_id])
     params[:location][:name].downcase!
     location = Location.update(params[:id], location_params)
     location.save
-    redirect_to profile_path
+    render_correct
   end
 
   private
+
+  def render_correct
+    case @user.role
+    when 'admin'
+      redirect_to admin_dashboard_path
+    when 'merchant'
+      redirect_to dashboard_path
+    else
+      redirect_to profile_path
+    end
+  end
 
   def location_params
     params.require(:location).permit(:name, :address, :city, :state, :zip)

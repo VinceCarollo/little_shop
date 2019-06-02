b231af5085abdf83361bd5cc279330070dd6de1c
Vince Carollo <vincecarollo@gmail.com>
2019-06-02 17:57:03 -0600

    Add optional relationship between orders and coupons

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,5 +1,6 @@
 class Coupon < ApplicationRecord
   belongs_to :user
+  has_many :orders
   validates :name, presence: true, uniqueness: true
   validates_presence_of :code, uniqueness: true
   validates :active, inclusion: { in: [ true, false ] }

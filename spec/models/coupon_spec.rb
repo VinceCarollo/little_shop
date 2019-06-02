d72f0e62934f4f59c8433f22daf2f778899ca283
Vince Carollo <vincecarollo@gmail.com>
2019-06-01 19:46:30 -0600

    Update coupon model to check if amount off is integer and add to model test

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Coupon, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :code }
-    it { should validate_presence_of :amount_off }
+    it { should validate_numericality_of(:amount_off).only_integer }
   end
 
   describe 'relationships' do

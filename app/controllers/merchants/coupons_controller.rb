23e171e6a76e54d1fbd92ff49218d954f1894085
Vince Carollo <vincecarollo@gmail.com>
2019-06-01 20:22:09 -0600

    Add edit, update, disable, enable, and destroy actions to merchants coupons controller

--- app/controllers/merchants/coupons_controller.rb
+++ app/controllers/merchants/coupons_controller.rb
@@ -12,7 +12,7 @@ class Merchants::CouponsController < Merchants::BaseController
   def create
     @user = current_user
     @coupon = @user.coupons.new(coupon_params)
-    if too_many_coupons?
+    if @user.coupons.length > 5
       flash.notice = "Sorry, you can have a max of 5 coupons in the system"
       redirect_to dashboard_coupons_path
     elsif @coupon.save
@@ -23,13 +23,43 @@ class Merchants::CouponsController < Merchants::BaseController
     end
   end
 
+  def edit
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def update
+    @coupon = Coupon.find(params[:id])
+    @coupon.update(coupon_params)
+    if @coupon.save
+      flash.notice = "Successfully Updated"
+      redirect_to dashboard_coupons_path
+    else
+      render :new
+    end
+  end
+
+  def disable
+    coupon = Coupon.find(params[:id])
+    coupon.update(active: false)
+    coupon.save
+    redirect_to dashboard_coupons_path
+  end
+
+  def enable
+    coupon = Coupon.find(params[:id])
+    coupon.update(active: true)
+    coupon.save
+    redirect_to dashboard_coupons_path
+  end
+
+  def destroy
+    Coupon.destroy(params[:id])
+    redirect_to dashboard_coupons_path
+  end
+
   private
 
   def coupon_params
     params.require(:coupon).permit(:name, :code, :amount_off)
-  end
-
-  def too_many_coupons?
-    @user.coupons.length >= 5
   end
 end

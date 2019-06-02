7110e7bc0a1461be2813d093d59e260d80791201
Vince Carollo <vincecarollo@gmail.com>
2019-06-02 16:18:52 -0600

    Add uniqueness check to code in coupon model

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,7 +1,7 @@
 class Coupon < ApplicationRecord
   belongs_to :user
   validates :name, presence: true, uniqueness: true
-  validates_presence_of :code
+  validates_presence_of :code, uniqueness: true
   validates :active, inclusion: { in: [ true, false ] }
   validates :amount_off, numericality: { only_integer: true }
 end

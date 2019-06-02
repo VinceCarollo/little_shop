217224aab069f3cda4fccf2c30c4c29622fcdeec
Vince Carollo <vincecarollo@gmail.com>
2019-06-02 17:56:33 -0600

    Add optional coupon_id attribute to orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190601233240) do
+ActiveRecord::Schema.define(version: 20190602234921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 20190601233240) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "location_id"
+    t.bigint "coupon_id"
+    t.index ["coupon_id"], name: "index_orders_on_coupon_id"
     t.index ["location_id"], name: "index_orders_on_location_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
@@ -88,6 +90,7 @@ ActiveRecord::Schema.define(version: 20190601233240) do
   add_foreign_key "locations", "users"
   add_foreign_key "order_items", "items"
   add_foreign_key "order_items", "orders"
+  add_foreign_key "orders", "coupons"
   add_foreign_key "orders", "locations"
   add_foreign_key "orders", "users"
 end

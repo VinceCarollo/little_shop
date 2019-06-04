e1dca44158cf4ab6ef21791221e999bf94ef2c83
Vince Carollo <vincecarollo@gmail.com>
2019-06-04 13:53:14 -0600

    Add update action to orders controller

--- app/controllers/default/orders_controller.rb
+++ app/controllers/default/orders_controller.rb
@@ -1,4 +1,20 @@
 class Default::OrdersController < Default::BaseController
+
+  def update
+    @order = Order.find(params[:id])
+    @user = current_user
+    @location = Location.find_by(user: @user, name: params[:location])
+    if used_in_completed_order?(@user, @order.location)
+      flash.notice = "This Order's address cannot be changed"
+      redirect_to(profile_order_path(@order))
+    else
+      @order.location = @location
+      @order.save
+      flash.notice = "This order's address was successfully changed"
+      redirect_to(profile_order_path(@order))
+    end
+  end
+
   def index
     @user = current_user
     @orders = @user.orders
@@ -7,6 +23,7 @@ class Default::OrdersController < Default::BaseController
   def show
     @order = Order.find(params[:id])
     @order_items = @order.order_items
+    @user = current_user
   end
 
   def destroy
@@ -39,5 +56,17 @@ class Default::OrdersController < Default::BaseController
       flash.notice = "Your Order Was Created"
       redirect_to profile_orders_path
     end
+  end
+
+  private
+
+  def used_in_completed_order?(user, location)
+    used = false
+    user.orders.each do |order|
+      if order.location == location
+        used = order.status == "shipped" || order.status == "packaged" || order.status == "shipped"
+      end
+    end
+    used
   end
 end
